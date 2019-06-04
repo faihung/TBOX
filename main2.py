de7e6dfc65c3ce1c830f40739f25e3a499b8b71b
yihuihong <huihong.yi@dingauto.cn>
2019-06-04 15:50:43 +0800

    程序大改之前先保存一份

--- main2.py
+++ main2.py
@@ -88,13 +88,13 @@ def config_client():
     s.connect(ip_port)  # 连接服务器
 
     while True:  # 通过一个死循环不断接收用户输入，并发送给服务器
-        inp = input("请输入要发送的信息： ").strip()
+        inp = input("Please enter the information to be sent: ").strip()
         if not inp:  # 防止输入空信息，导致异常退出
             continue
         s.sendall(inp.encode())
 
         if inp == "exit":  # 如果输入的是‘exit’，表示断开连接
-            print("结束通信！")
+            # print("结束通信！")
             break
 
         server_reply = s.recv(1024).decode()
