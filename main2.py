d082c04552e6191c4ba9bf35dab9f5117270eebd
yihuihong <huihong.yi@dingauto.cn>
2019-06-04 11:13:53 +0800

    接收数据部分改为UDP服务端，配置网络调试工具ok

--- main2.py
+++ main2.py
@@ -12,78 +12,93 @@ FORMAT_MESSAGE = "{channel}  {id:<15} Rx   {dtype} {data}"
 FORMAT_DATE = "%a %b %m %I:%M:%S %p %Y"
 FORMAT_EVENT = "{timestamp: 9.4f} {message}\n"
 
-
-def recorder_server():
-    ip_port = ('192.168.0.200', 7)
-    # ip_port = ('127.0.0.1', 6666)
-    s = socket.socket()  # 创建套接字
-    s.connect(ip_port)  # 连接服务器
-
-    # the last part is written with the timestamp of the first message
-    timestamp = None
-    header_written = False
-    last_timestamp = None
-    started = None
+def recorder_udp_server():
+    ip_port = ('192.168.0.221', 6666)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+    s.bind(ip_port)
 
     while True:
-        #ASCII码字符串
-        server_reply = s.recv(1024).decode()
-        # print("server_reply: %s" % server_reply)
+        data = s.recv(1024).strip().decode()
+        print("hello1")
+        print(data)
+        print("hello2")
+        if data == "exit":
+            print("客户端主动断开连接！")
+            break
 
-        #ASCII码字符串转换为hex string
-        server_reply2 = binascii.hexlify("".join(server_reply).encode()).decode()#
-        # print("server_reply2: %s" % server_reply2)
+    s.close()
 
-        #hex string转换为整形列表
-        server_reply3 = bytearray.fromhex(server_reply2)
-        server_reply4 = list(server_reply3)
-        # print("server_reply4: %s" % server_reply4)#server_reply4: [0, 48, 49, 50, 51, 52, 53, 54]
-        with open(r'F:\yhh\0-Source\work\work_data_collect_record\TBOX\data\test.txt', 'a') as f:
-            # this is the case for the very first message:
-            if not header_written:
-                # write start of file header
-                now = datetime.now().strftime("%a %b %m %I:%M:%S %p %Y")
-                f.write("date %s\n" % now)
-                f.write("base hex  timestamps absolute\n")
-                f.write("internal events logged\n")
-
-                last_timestamp = (timestamp or 0.0)
-                started = last_timestamp
-                formatted_date = time.strftime(FORMAT_DATE, time.localtime(last_timestamp))
-                f.write("Begin Triggerblock %s\n" % formatted_date)
-                header_written = True
-                f.write("0.0000 Start of measurement\n")  # caution: this is a recursive call!
-
-            # figure out the correct timestamp
-            if timestamp is None:
-                timestamp = last_timestamp
-                timestamp = time.time()
-                started = timestamp
-            else:
-                timestamp = time.time()
-                timestamp -= started
-                f.write("%0.4f" % timestamp)
-                f.write(' ')
-                f.write('can0:')
-                f.write(' ')
-                # can id
-                f.write(' ')
-                f.write('Rx')
-                f.write(' ')
-                f.write('d')
-                f.write(' ')
-                f.write('8')
-                f.write(' ')
-                for i in range(len(server_reply4)):
-                    f.write(str(hex(server_reply4[i]))[2:])
-                    f.write(' ')
-                f.write('\n')
-
-            # turn into relative timestamps if necessary
-            if timestamp >= started:
-                started = timestamp
-
-    s.close()  # 关闭连接
+# def recorder_server():
+#     ip_port = ('192.168.0.200', 7)
+#     # ip_port = ('127.0.0.1', 6666)
+#     s = socket.socket()  # 创建套接字
+#     s.connect(ip_port)  # 连接服务器
+#
+#     # the last part is written with the timestamp of the first message
+#     timestamp = None
+#     header_written = False
+#     last_timestamp = None
+#     started = None
+#
+#     while True:
+#         #ASCII码字符串
+#         server_reply = s.recv(1024).decode()
+#         # print("server_reply: %s" % server_reply)
+#
+#         #ASCII码字符串转换为hex string
+#         server_reply2 = binascii.hexlify("".join(server_reply).encode()).decode()#
+#         # print("server_reply2: %s" % server_reply2)
+#
+#         #hex string转换为整形列表
+#         server_reply3 = bytearray.fromhex(server_reply2)
+#         server_reply4 = list(server_reply3)
+#         # print("server_reply4: %s" % server_reply4)#server_reply4: [0, 48, 49, 50, 51, 52, 53, 54]
+#         with open(r'F:\yhh\0-Source\work\work_data_collect_record\TBOX\data\test.txt', 'a') as f:
+#             # this is the case for the very first message:
+#             if not header_written:
+#                 # write start of file header
+#                 now = datetime.now().strftime("%a %b %m %I:%M:%S %p %Y")
+#                 f.write("date %s\n" % now)
+#                 f.write("base hex  timestamps absolute\n")
+#                 f.write("internal events logged\n")
+#
+#                 last_timestamp = (timestamp or 0.0)
+#                 started = last_timestamp
+#                 formatted_date = time.strftime(FORMAT_DATE, time.localtime(last_timestamp))
+#                 f.write("Begin Triggerblock %s\n" % formatted_date)
+#                 header_written = True
+#                 f.write("0.0000 Start of measurement\n")  # caution: this is a recursive call!
+#
+#             # figure out the correct timestamp
+#             if timestamp is None:
+#                 timestamp = last_timestamp
+#                 timestamp = time.time()
+#                 started = timestamp
+#             else:
+#                 timestamp = time.time()
+#                 timestamp -= started
+#                 f.write("%0.4f" % timestamp)
+#                 f.write(' ')
+#                 f.write('can0:')
+#                 f.write(' ')
+#                 # can id
+#                 f.write(' ')
+#                 f.write('Rx')
+#                 f.write(' ')
+#                 f.write('d')
+#                 f.write(' ')
+#                 f.write('8')
+#                 f.write(' ')
+#                 for i in range(len(server_reply4)):
+#                     f.write(str(hex(server_reply4[i]))[2:])
+#                     f.write(' ')
+#                 f.write('\n')
+#
+#             # turn into relative timestamps if necessary
+#             if timestamp >= started:
+#                 started = timestamp
+#
+#     s.close()  # 关闭连接
 
 def config_client():
     ip_port = ('192.168.0.200', 8)
@@ -106,9 +121,9 @@ def config_client():
     s.close()  # 关闭连接
 
 if __name__ == '__main__':
-    t_server = threading.Thread(target=recorder_server, name='Record-Thread', args=())
+    t_server = threading.Thread(target=recorder_udp_server, name='Record-Thread', args=())
+    # t_server = threading.Thread(target=recorder_server, name='Record-Thread', args=())
     t_client = threading.Thread(target=config_client, name='Config-Thread', args=())
 
     t_server.start()
     t_client.start()
-
