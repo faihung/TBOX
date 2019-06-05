1bb3554b07d522dc83a6fd5a80580afc57f1d27b
yihuihong <huihong.yi@dingauto.cn>
2019-06-05 21:11:33 +0800

    window环境下可以接收ui命令

--- main2.py
+++ main2.py
@@ -72,79 +72,79 @@ def DUT_receive_UI(body):
 
 
 '''-----------------------------------------------------------recorder_udp_server-----------------------------------------------------------'''
-# def recorder_udp_server():
-#     ip_port = ('192.168.0.222', 6666)
-#     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
-#     s.bind(ip_port)
-#
-#     # the last part is written with the timestamp of the first message
-#     timestamp = None
-#     header_written = False
-#     last_timestamp = None
-#     started = None
-#
-#     while True:
-#         data = s.recv(1024).strip().decode()
-#         # print(data)
-#         server_reply2 = binascii.hexlify("".join(data).encode()).decode()  #
-#         # print("server_reply2: %s" % server_reply2)
-#
-#         server_reply3 = bytearray.fromhex(server_reply2)
-#         server_reply4 = list(server_reply3)
-#         # print("server_reply4: %s" % server_reply4)#server_reply4: [0, 48, 49, 50, 51, 52, 53, 54]
-#
-#         with open('/home/root/test.asc', 'a') as f:
-#         # with open(r'F:\yhh\0-Source\work\work_data_collect_record\TBOX\data\test.txt', 'a') as f:
-#             # this is the case for the very first message:
-#             if not header_written:
-#                 # write start of file header
-#                 now = datetime.now().strftime("%a %b %m %I:%M:%S %p %Y")
-#                 f.write("date %s\n" % now)
-#                 f.write("base hex  timestamps absolute\n")
-#                 f.write("internal events logged\n")
-#
-#                 last_timestamp = (timestamp or 0.0)
-#                 started = last_timestamp
-#                 formatted_date = time.strftime(FORMAT_DATE, time.localtime(last_timestamp))
-#                 f.write("Begin Triggerblock %s\n" % formatted_date)
-#                 header_written = True
-#                 f.write("0.0000 Start of measurement\n")  # caution: this is a recursive call!
-#
-#             # figure out the correct timestamp
-#             if timestamp is None:
-#                 timestamp = last_timestamp
-#                 timestamp = time.time()
-#                 started = timestamp
-#             else:
-#                 timestamp = time.time()
-#                 timestamp -= started
-#                 f.write("%0.4f" % timestamp)
-#                 f.write(' ')
-#                 f.write('can0:')
-#                 f.write(' ')
-#                 # can id
-#                 f.write(' ')
-#                 f.write('Rx')
-#                 f.write(' ')
-#                 f.write('d')
-#                 f.write(' ')
-#                 f.write('8')
-#                 f.write(' ')
-#                 for i in range(len(server_reply4)):
-#                     f.write(str(hex(server_reply4[i]))[2:])
-#                     f.write(' ')
-#                 f.write('\n')
-#
-#             # turn into relative timestamps if necessary
-#             if timestamp >= started:
-#                 started = timestamp
-#
-#     s.close() # 关闭连接
+def recorder_udp_server():
+    ip_port = ('192.168.0.221', 6666)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+    s.bind(ip_port)
+
+    # the last part is written with the timestamp of the first message
+    timestamp = None
+    header_written = False
+    last_timestamp = None
+    started = None
+
+    while True:
+        data = s.recv(1024).strip().decode()
+        # print(data)
+        server_reply2 = binascii.hexlify("".join(data).encode()).decode()  #
+        # print("server_reply2: %s" % server_reply2)
+
+        server_reply3 = bytearray.fromhex(server_reply2)
+        server_reply4 = list(server_reply3)
+        # print("server_reply4: %s" % server_reply4)#server_reply4: [0, 48, 49, 50, 51, 52, 53, 54]
+
+        # with open('/home/root/test.asc', 'a') as f:
+        with open(r'F:\yhh\0-Source\work\work_data_collect_record\TBOX\data\test.txt', 'a') as f:
+            # this is the case for the very first message:
+            if not header_written:
+                # write start of file header
+                now = datetime.now().strftime("%a %b %m %I:%M:%S %p %Y")
+                f.write("date %s\n" % now)
+                f.write("base hex  timestamps absolute\n")
+                f.write("internal events logged\n")
+
+                last_timestamp = (timestamp or 0.0)
+                started = last_timestamp
+                formatted_date = time.strftime(FORMAT_DATE, time.localtime(last_timestamp))
+                f.write("Begin Triggerblock %s\n" % formatted_date)
+                header_written = True
+                f.write("0.0000 Start of measurement\n")  # caution: this is a recursive call!
+
+            # figure out the correct timestamp
+            if timestamp is None:
+                timestamp = last_timestamp
+                timestamp = time.time()
+                started = timestamp
+            else:
+                timestamp = time.time()
+                timestamp -= started
+                f.write("%0.4f" % timestamp)
+                f.write(' ')
+                f.write('can0:')
+                f.write(' ')
+                # can id
+                f.write(' ')
+                f.write('Rx')
+                f.write(' ')
+                f.write('d')
+                f.write(' ')
+                f.write('8')
+                f.write(' ')
+                for i in range(len(server_reply4)):
+                    f.write(str(hex(server_reply4[i]))[2:])
+                    f.write(' ')
+                f.write('\n')
+
+            # turn into relative timestamps if necessary
+            if timestamp >= started:
+                started = timestamp
+
+    s.close() # 关闭连接
 
 '''-----------------------------------------------------------config_client-----------------------------------------------------------'''
 
 def mpc5748_process(Mpc5748Cmd_channel, props, body, mpc5748cmd_temp):
-    ip_port = ('127.0.0.1', 6666)#ip_port = ('192.168.0.200', 8)  #
+    ip_port = ('192.168.0.200', 8)  #ip_port = ('127.0.0.1', 6666)#
     s = socket.socket()
     s.connect(ip_port)
     inp = "On,12345678,Add".encode()
@@ -202,9 +202,9 @@ if __name__ == '__main__':
     Mpc5748Cmd_channel = Mpc5748Cmd_connection.channel()
 
     # t0 = threading.Thread(target=heartbeat, name='HeartBeat-Thread', args=())
-    # t_server = threading.Thread(target=recorder_udp_server, name='Record-Thread', args=())
+    t_server = threading.Thread(target=recorder_udp_server, name='Record-Thread', args=())
     t_client = threading.Thread(target=config_client, name='Config-Thread', args=())
 
     # t0.start()
-    # t_server.start()
+    t_server.start()
     t_client.start()
